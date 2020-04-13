7688189d3d0be452cdc40fededdd7ea3d077961d
Marcelo Mottalli <mmottalli@zendesk.com>
2020-04-13 16:39:59 +0100

    Refactoring

--- src/main/scala/server/Main.scala
+++ src/main/scala/server/Main.scala
@@ -46,80 +46,7 @@ object GraphQLSchema {
     )
   )
 
-  val schema = Schema(QueryType)
-}
-
-class GraphQLServer extends LazyLogging {
-  private def executeGraphQLQuery(query: Document, operation: Option[String], variables: Json)(
-    implicit ec: ExecutionContext
-  ) =
-    Executor.execute(GraphQLSchema.schema, query, new FooRepository, variables = variables, operationName = operation)
-
-  def formatError(error: Throwable): Json = error match {
-    case syntaxError: SyntaxError ⇒
-      Json.obj(
-        "errors" → Json.arr(
-          Json.obj(
-            "message" → Json.fromString(syntaxError.getMessage),
-            "locations" → Json.arr(
-              Json.obj(
-                "line" → Json.fromBigInt(syntaxError.originalError.position.line),
-                "column" → Json.fromBigInt(syntaxError.originalError.position.column)
-              )
-            )
-          )
-        )
-      )
-    case NonFatal(e) ⇒
-      formatError(e.getMessage)
-    case e ⇒
-      throw e
-  }
-
-  private def formatError(message: String): Json =
-    Json.obj("errors" -> Json.arr(Json.obj("message" -> Json.fromString(message))))
-
-  def endpoint(implicit ec: ExecutionContext): Route =
-    post {
-      entity(as[Json]) { requestJson =>
-        val queryOpt: Option[String] = root.selectDynamic("query").string.getOption(requestJson)
-
-        queryOpt match {
-          case None => complete(StatusCodes.BadRequest, formatError("No GraphQL query to execute"))
-          case Some(query) =>
-            QueryParser.parse(query) match {
-              case Failure(error) => complete(StatusCodes.BadRequest, formatError(error))
-              case Success(queryAst) =>
-                val operationName: Option[String] = root.selectDynamic("operationName").string.getOption(requestJson)
-                val variables: Json = root.selectDynamic("variables").json.getOption(requestJson).getOrElse(Json.obj())
-                complete(executeGraphQLQuery(queryAst, operationName, variables))
-            }
-        }
-      }
-    } ~
-      get { getFromResource("playground.html") }
-}
-
-class Server(settings: Settings) extends LazyLogging {
-
-  implicit lazy val system: ActorSystem = ActorSystem("sangria-graphql-server")
-  implicit lazy val materializer: ActorMaterializer = ActorMaterializer()
-
-  import system.dispatcher
-
-  lazy val graphQLServer = new GraphQLServer
-
-  lazy val route: Route =
-    path("graphql") {
-      graphQLServer.endpoint
-    } ~
-      (get & pathEndOrSingleSlash) {
-        redirect("/graphql", StatusCodes.PermanentRedirect)
-      }
-
-  def serve(): Unit = {
-    Http().bindAndHandle(route, "0.0.0.0", settings.port)
-  }
+  val schema: Schema[FooRepository, Unit] = Schema(QueryType)
 }
 
 object Main {
@@ -127,7 +54,10 @@ object Main {
     import com.softwaremill.macwire._
 
     lazy val settings: Settings = new Settings()
-    lazy val server: Server = wire[Server]
+    lazy val context: FooRepository = new FooRepository()
+    lazy val schema: Schema[FooRepository, Unit] = GraphQLSchema.schema
+    lazy val graphQLServer: GraphQLServer[FooRepository] = wire[GraphQLServer[FooRepository]]
+    lazy val server: Server[FooRepository] = wire[Server[FooRepository]]
 
     server.serve()
   }
